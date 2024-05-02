da1401e4323011f1d8e726c1379e98730fa3475e
jhon.artuz <jhonreyartuz5@gmail.com>
2024-05-02 15:34:16 +0800

    update the code and libraries

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,17 @@ import Main from './Main'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      token: {},
+      ethSwap: {},
+      ethBalance: '0',
+      tokenBalance: '0',
+      loading: true
+    }
+  }
 
   async componentWillMount() {
     await this.loadWeb3()
@@ -14,42 +25,52 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
-
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-
-    const ethBalance = await web3.eth.getBalance(this.state.account)
-    this.setState({ ethBalance })
-
-    // Load Token
-    const networkId =  await web3.eth.net.getId()
-    const tokenData = Token.networks[networkId]
-    if(tokenData) {
-      const token = new web3.eth.Contract(Token.abi, tokenData.address)
-      this.setState({ token })
-      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
-      this.setState({ tokenBalance: tokenBalance.toString() })
-    } else {
-      window.alert('Token contract not deployed to detected network.')
+    if (!window.ethereum || !window.ethereum.isMetaMask) {
+      window.alert('MetaMask is not installed or not properly configured.');
+      return;
     }
-
-    // Load EthSwap
-    const ethSwapData = EthSwap.networks[networkId]
-    if(ethSwapData) {
-      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
-      this.setState({ ethSwap })
-    } else {
-      window.alert('EthSwap contract not deployed to detected network.')
+  
+    const web3 = new Web3(window.ethereum);
+  
+    try {
+      const accounts = await web3.eth.requestAccounts();
+      this.setState({ account: accounts[0] });
+  
+      const ethBalance = await web3.eth.getBalance(this.state.account);
+      this.setState({ ethBalance });
+  
+      // Load Token
+      const networkId = await web3.eth.net.getId();
+      const tokenData = Token.networks[networkId];
+      if (tokenData) {
+        const token = new web3.eth.Contract(Token.abi, tokenData.address);
+        this.setState({ token });
+        let tokenBalance = await token.methods.balanceOf(this.state.account).call();
+        this.setState({ tokenBalance: tokenBalance.toString() });
+      } else {
+        window.alert('Token contract not deployed to detected network.');
+      }
+  
+      // Load EthSwap
+      const ethSwapData = EthSwap.networks[networkId];
+      if (ethSwapData) {
+        const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+        this.setState({ ethSwap });
+      } else {
+        window.alert('EthSwap contract not deployed to detected network.');
+      }
+  
+      this.setState({ loading: false });
+    } catch (error) {
+      console.error('Error loading blockchain data:', error);
+      window.alert('An error occurred while loading blockchain data. Please check the console for details.');
     }
-
-    this.setState({ loading: false })
   }
+    
 
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
     }
     else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
@@ -73,18 +94,6 @@ class App extends Component {
         this.setState({ loading: false })
       })
     })
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      token: {},
-      ethSwap: {},
-      ethBalance: '0',
-      tokenBalance: '0',
-      loading: true
-    }
   }
 
   render() {
